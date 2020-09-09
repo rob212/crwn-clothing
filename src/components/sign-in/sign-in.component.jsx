ce2b2f29528f074771e7e077190c6ffee05c102b
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-09-09 11:24:26 +0100

    added the ability to sign up with email and password combo

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -40,7 +40,7 @@ class SignIn extends React.Component {
             name="email"
             value={this.state.email}
             handleChange={this.handleChange}
-            label="email"
+            label="Email"
             required
           ></FormInput>
 
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
             name="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label="Password"
             required
           ></FormInput>
 
