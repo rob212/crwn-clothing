8063cfa70d695bbf6c6d5f237fd3fa86147f996d
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-09-14 14:30:56 +0100

    added UserActionTypes constants and redirected signed in user from sign in page.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from './redux/user/user.actions';
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 class App extends React.Component {
 
@@ -45,7 +46,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/shop" component={ShopPage} />
-            <Route exact path="/signin" component={SignInAndSignUpPage} />
+            <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
           </Switch>
         </main>
       </div>
@@ -53,8 +54,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
